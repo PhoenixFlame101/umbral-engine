80c34c55316d9115bf64af7afdb1c3392f01b0e7
Abhinav Chennubhotla <51199011+PhoenixFlame101@users.noreply.github.com>
2024-01-24 19:52:51 +0530

    feat: swap shaders on pressing spacebar

--- src/lib.rs
+++ src/lib.rs
@@ -170,7 +170,72 @@ impl State {
 
     #[allow(unused_variables)]
     fn input(&mut self, event: &WindowEvent) -> bool {
-        false
+        match event {
+            WindowEvent::KeyboardInput { input, .. } => match input {
+                KeyboardInput {
+                    virtual_keycode, ..
+                } => match virtual_keycode.unwrap() {
+                    VirtualKeyCode::Space => {
+                        let shader = self
+                            .device
+                            .create_shader_module(wgpu::include_wgsl!("shader2.wgsl"));
+
+                        let render_pipeline_layout =
+                            self.device
+                                .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                                    label: Some("Render Pipeline Layout"),
+                                    bind_group_layouts: &[],
+                                    push_constant_ranges: &[],
+                                });
+
+                        let render_pipeline =
+                            self.device
+                                .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                                    label: Some("Render Pipeline"),
+                                    layout: Some(&render_pipeline_layout),
+                                    vertex: wgpu::VertexState {
+                                        module: &shader,
+                                        entry_point: "vs_main",
+                                        buffers: &[],
+                                    },
+                                    fragment: Some(wgpu::FragmentState {
+                                        module: &shader,
+                                        entry_point: "fs_main",
+                                        targets: &[Some(wgpu::ColorTargetState {
+                                            format: self.config.format,
+                                            blend: Some(wgpu::BlendState::REPLACE),
+                                            write_mask: wgpu::ColorWrites::ALL,
+                                        })],
+                                    }),
+                                    primitive: wgpu::PrimitiveState {
+                                        topology: wgpu::PrimitiveTopology::TriangleList,
+                                        strip_index_format: None,
+                                        front_face: wgpu::FrontFace::Ccw,
+                                        cull_mode: Some(wgpu::Face::Back),
+                                        // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                                        polygon_mode: wgpu::PolygonMode::Fill,
+                                        // Requires Features::DEPTH_CLIP_CONTROL
+                                        unclipped_depth: false,
+                                        // Requires Features::CONSERVATIVE_RASTERIZATION
+                                        conservative: false,
+                                    },
+                                    depth_stencil: None,
+                                    multisample: wgpu::MultisampleState {
+                                        count: 1,
+                                        mask: !0,
+                                        alpha_to_coverage_enabled: false,
+                                    },
+                                    multiview: None,
+                                });
+
+                        self.render_pipeline = render_pipeline;
+                        true
+                    }
+                    _ => false,
+                },
+            },
+            _ => false,
+        }
     }
 
     fn update(&mut self) {}
---
+++ src/shader2.wgsl
@@ -0,0 +1,25 @@
+// Vertex shader
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>,
+};
+
+@vertex
+fn vs_main(
+    @builtin(vertex_index) in_vertex_index: u32,
+) -> VertexOutput {
+    var out: VertexOutput;
+    let x = f32(1 - i32(in_vertex_index)) * 0.5;
+    let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.5;
+    out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
+    return out;
+}
+
+
+// Fragment shader
+@fragment
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    let r = 0.3; // R = 54
+    let g = 0.5 ; // G = 201
+    let b = 0.3; // B = 152
+    return vec4<f32>(r, g, b, 1.0);
+}
